29c84d70c997e6b1f0105b5dbaf7126523099ced
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-05-02 16:09:28 +0000

    enrollments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :courses
+  has_many :enrollments
+  has_many :enrolled_courses, through: :enrollments, source: :course
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def enrolled_in?(course)
+    return enrolled_courses.include?(course)
+  end
 end
