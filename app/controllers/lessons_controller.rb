79795e065019fe8195421ebdb9730df0aae122f4
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-05-02 19:19:21 +0000

    enrollment check

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_course, only: [:show]
+  
   def show 
   end
 
@@ -6,5 +9,11 @@ class LessonsController < ApplicationController
     helper_method :current_lesson
     def current_lesson
       @current_lesson ||= Lesson.find(params[:id])
+    end
+
+    def require_enrolled_for_course
+      if !current_user.enrolled_in?(current_lesson.section.course)
+        redirect_to course_path, alert: 'You must be enrolled to view the lessons.'
+      end
     end
 end
