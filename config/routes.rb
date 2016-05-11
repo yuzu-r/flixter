947f00b921934388a5cdad02bae439862714cd2d
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-05-11 17:18:41 +0000

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Flixter::Application.routes.draw do
   devise_for :users
   root      'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'team', to: 'static_pages#team'
+  get 'careers', to: 'static_pages#careers'
+  
   resource :dashboard, only: [:show]
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
